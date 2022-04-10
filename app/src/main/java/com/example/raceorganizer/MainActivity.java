910115ec92367fe74cb51a2f6b752bf425602671
hanchry <jurco10510@gmail.com>
2022-04-10 14:31:49 +0200

    setting up of navigation and theme colors home page and login/register problem with having 2 resorces somehow

--- app/src/main/java/com/example/raceorganizer/MainActivity.java
+++ app/src/main/java/com/example/raceorganizer/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.raceorganizer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavController navController;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setup();
+
+
+        Button organize = findViewById(R.id.organize);
+        organize.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                navController.navigate(R.id.login_register2);
+
+            }
+        });
+
     }
+
+    private void setup(){
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/raceorganizer/login_register.java
@@ -0,0 +1,25 @@
+package com.example.raceorganizer;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class login_register extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login_register,
+                container, false);
+
+        return inflater.inflate(R.layout.fragment_login_register, container, false);
+    }
+}
