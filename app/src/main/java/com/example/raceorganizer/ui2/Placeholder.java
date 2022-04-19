eb03fd4aef245caddbd562891f58b683110e995c
lukisko <lukas.pleva@mail.com>
2022-04-19 10:31:16 +0200

    make add participant view and its viewModel

---
+++ app/src/main/java/com/example/raceorganizer/ui2/Placeholder.java
@@ -0,0 +1,4 @@
+package com.example.raceorganizer.ui2;
+
+public class Placeholder {
+}
---
+++ app/src/main/java/com/example/raceorganizer/ui2/addParticipant/AddParticipantView.java
@@ -0,0 +1,42 @@
+package com.example.raceorganizer.ui2.addParticipant;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.raceorganizer.R;
+
+public class AddParticipantView extends AppCompatActivity {
+    private AddParticipantViewModel viewModel;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        viewModel = new ViewModelProvider(this).get(AddParticipantViewModel.class);
+
+        EditText firstName = findViewById(R.id.participantFirstName);
+        EditText lastName = findViewById(R.id.participantLastName);
+        EditText age = findViewById(R.id.participantAge);
+        EditText number = findViewById(R.id.participantNumber);
+
+        firstName.setText(viewModel.getFirstName());
+        lastName.setText(viewModel.getLastName());
+        age.setText(viewModel.getAge());
+        number.setText(viewModel.getNumber());
+
+        Button createButton = findViewById(R.id.addParticipant);
+        createButton.setOnClickListener((v)->{
+            viewModel.setAge(age.getText().toString());
+            viewModel.setFirstName(firstName.getText().toString());
+            viewModel.setLastName(lastName.getText().toString());
+            viewModel.setNumber(number.getText().toString());
+
+            viewModel.makeParticipant();
+        });
+    }
+}
